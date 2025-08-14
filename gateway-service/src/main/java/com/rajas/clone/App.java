078ca3735eb6b64b5cc94cf716ebcae3693d9772
Dileep <vrdileep1999@gmail.com>
2025-08-14 20:23:26 +0530

    Added parent Maven project and gateway service module

---
+++ gateway-service/src/main/java/com/rajas/clone/App.java
@@ -0,0 +1,13 @@
+package com.rajas.clone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
